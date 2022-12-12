eb6c9af19c565343fade9ea0550b32747220e2fa
justyn <marie.mbarga@isl-edu.be>
2022-12-12 19:27:36 +0100

    UH

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,7 +8,8 @@ use App\Form\CategoryType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Faker\Factory;
 
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class CategoryType extends AbstractType
 {
@@ -17,7 +19,9 @@ class CategoryType extends AbstractType
             ->add('name',TextType::class,
             [
                 'label'=>'Nom de la catégorie',
+
                 'required' => true
+
             ])
           ->add('submit',SubmitType::class,
             [
@@ -27,6 +31,7 @@ class CategoryType extends AbstractType
 
         ;
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
